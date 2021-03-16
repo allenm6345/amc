d40fca1935b63c33b0146973809af6dbfb749b4c
allenm6345 <75119213+allenm6345@users.noreply.github.com>
2021-03-15 20:52:04 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var num1=((Math.random()*52)+1); num1=(Math.floor(num1));
+
+document.querySelector("h1").textContent=(num1);
+
+var num2=((Math.random()*52)+1); num2=(Math.floor(num2));
+
+document.querySelector("h1").textContent=""+num1+("   ")+num2+"";
