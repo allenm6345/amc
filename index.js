17baa736defb03fb555c353ceaec6ddc3ddd7e7a
allenm6345 <75119213+allenm6345@users.noreply.github.com>
2021-03-18 17:26:53 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -10,5 +10,18 @@ var randomKardSource="images/"+randomKardImage;
 
 dorf=document.querySelectorAll("img")[0];
 
+dorf.setAttribute("src",randomKardSource);
+
+var num1=((Math.random()*52)+1); num1=(Math.floor(num1));
+
+var randomKardImage =num1 + ".jpg";
+
+// alert(randomKardImage);
+
+var randomKardSource="images/"+randomKardImage;
+
+// alert(randomKardSource);
+
+dorf=document.querySelectorAll("img")[1];
 
 dorf.setAttribute("src",randomKardSource);
